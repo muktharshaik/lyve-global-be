195aa110a2ac0d53923127c057751f95b8fb0692
mukthar <msshaikmukthar9@gmail.com>
2023-09-13 17:05:53 +0400

    Added json dummy data and data types

---
+++ src/types/dataTypes.ts
@@ -0,0 +1,37 @@
+export interface CustomizationVariant {
+  name: string;
+  price: number | null;
+  image?: string;
+}
+
+export interface CustomizationOption {
+  primaryText: string;
+  secondaryText: string;
+  isRequired: boolean;
+  variants: CustomizationVariant[];
+}
+
+export interface Item {
+  itemId: string;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  customizations: CustomizationOption[];
+}
+
+export interface Category {
+  categoryId: string;
+  categoryName: string;
+  items: Item[];
+}
+
+export interface Restaurant {
+  id: string;
+  name: string;
+  image: string;
+  deliveryTime: string;
+  costRange: number;
+  rating: number;
+  categories: Category[];
+}
