9e41c6a8715a5e3f473594118ea0ce034b091339
mukthar <msshaikmukthar9@gmail.com>
2023-09-14 21:13:25 +0400

    cors initialized

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { categoryRoutes, itemsRoutes, restaurantRoutes } from '../components';
+import cors from 'cors';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/api/restaurants', restaurantRoutes);
 app.use('/api/category', categoryRoutes);
